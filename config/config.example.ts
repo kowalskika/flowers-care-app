13a8cf12ad2750b8fa35f8d7968f1145cdc8ff26
kowalskika <125073360+kowalskika@users.noreply.github.com>
2023-04-03 00:09:32 +0200

    Updated config.example file.

--- config/config.example.ts
+++ config/config.example.ts
@@ -5,5 +5,8 @@
  *   dbPassword: 'password',
  *   dbDatabase: 'database',
  *   corsOrigin: 'http://localhost:3000',
+ *   ALLOWED_ORIGINS: ['http://localhost:3000'],
+ *   ACCESS_TOKEN_SECRET: '',
+ *   REFRESH_TOKEN_SECRET: '',
  * };
  */
