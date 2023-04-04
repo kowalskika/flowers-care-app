8a86429bd94a8cc4d9d4c48e13d2fd42123ecb6c
kowalskika <125073360+kowalskika@users.noreply.github.com>
2023-04-04 16:18:09 +0200

    Added types for form - fe.

---
+++ types/flower/flower.form.ts
@@ -0,0 +1,9 @@
+export enum FlowerUpdateForm {
+  name = 'name',
+  wateredAt = 'wateredAt',
+  info = 'info',
+  species = 'species',
+  replantedAt = 'replantedAt',
+  fertilizedAt = 'fertilizedAt',
+  wateringInterval = 'wateringInterval',
+}
