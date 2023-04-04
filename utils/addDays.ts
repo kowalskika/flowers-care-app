d80cfd860ced68a2de1395196c340b56fc8853ff
kowalskika <125073360+kowalskika@users.noreply.github.com>
2023-04-04 17:12:08 +0200

    Separated addDays into addDaysToLocaleDateString and addDaysToDbString.

--- utils/addDays.ts
+++ utils/addDays.ts
@@ -1,5 +1,11 @@
-export function addDays(date: Date, days: number) {
+export function addDaysToDbString(date: Date, days: number) {
   const copy = new Date(Number(date));
   copy.setDate(date.getDate() + days);
-  return copy;
+  return copy.toISOString().slice(0, 19).replace('T', ' ');
+}
+
+export function addDaysToLocaleDateString(date: Date, days: number) {
+  const copy = new Date(Number(date));
+  copy.setDate(date.getDate() + days);
+  return copy.toLocaleDateString('fr-CH');
 }
