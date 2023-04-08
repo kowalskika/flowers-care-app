abc584a322495fe44d01ec0064f0613d1e6f7e5d
kowalskika <125073360+kowalskika@users.noreply.github.com>
2023-04-08 15:10:37 +0200

    Updated config example file.

--- routes/home.router.ts
+++ routes/home.router.ts
@@ -5,4 +5,7 @@ export const homeRouter = express.Router();
 homeRouter
   .get('/', (req, res) => {
     res.redirect('/flower');
+  })
+  .get('/xx', (req, res) => {
+    res.send('DZIAŁA TO FOKLE?');
   });
